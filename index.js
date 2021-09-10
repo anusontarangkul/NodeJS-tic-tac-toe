71dc989aa061880833111ab3f9f535900aa2e080
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-09 20:46:12 -0700

    refactored and add comments

--- index.js
+++ index.js
@@ -4,27 +4,20 @@ let players = ["X", "O"]
 let playerXTurn = false;
 let playerOTurn = false;
 
-let board = [[1, 2, 3],
-[4, 5, 6],
-[7, 8, 9]];
+let board = []
 
 function startGame() {
-    let gameOver = false;
 
+    board = [[1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]];
 
-
-    // console.log("", test[0], '\n', test[1], '\n', test[2], '\n')
     console.log('\n')
     console.log("New Game Of Tic Tac Toe!")
     console.log('\n')
     randomizeStartingPlayer();
-    // console.log(`Player ${firstPlayer} goes first.`)
-
 
     decideTurn();
-
-
-
 
 }
 
@@ -39,10 +32,9 @@ function randomizeStartingPlayer() {
 }
 
 async function playerMove(player) {
-    console.log(`Player ${player} turn:`)
+
+    // Create array of available moves for player
     let avaibleMoves = [];
-    console.log('\n')
-    console.log("", board[0], '\n', board[1], '\n', board[2], '\n')
     for (let row of board) {
         for (let i = 0; i < row.length; i++) {
             if (row[i] !== "O" && row[i] !== "X") {
@@ -50,19 +42,30 @@ async function playerMove(player) {
             }
         }
     }
+    // Check if game is a draw
+    if (avaibleMoves.length === 0) {
+        console.log("Game is a draw.")
+        console.log('\n')
+        return playagain()
+    }
+    console.log('\n')
+    console.log(`Player ${player} turn:`)
+
+    console.log('\n')
+    console.log("", board[0], '\n', board[1], '\n', board[2], '\n')
 
     const inputChoice = await inquirer
         .prompt([
             {
                 type: 'list',
-                name: 'name',
+                name: 'arrayIndex',
                 message: 'Which spot do you want?',
                 choices: avaibleMoves
             }
         ])
     for (let row of board) {
         for (let i = 0; i < row.length; i++) {
-            if (row[i] === inputChoice.name) {
+            if (row[i] === inputChoice.arrayIndex) {
                 row[i] = player
             }
         }
@@ -76,46 +79,88 @@ function checkWinner(player) {
 
 
 
-
+    // first column win
     if (board[0][0] === board[1][0] && board[0][0] === board[2][0]) {
         console.log(`player ${player} wins!`)
+        console.log('\n')
+        return playagain()
     }
+    // second column win
     if (board[0][1] === board[1][1] && board[0][1] === board[2][1]) {
         console.log(`player ${player} wins!`)
+        console.log('\n')
+        return playagain()
     }
+    // third column win
     if (board[0][2] === board[1][2] && board[0][2] === board[2][2]) {
         console.log(`player ${player} wins!`)
+        console.log('\n')
+        return playagain()
     }
+    // diagnol left-right win
     if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
         console.log(`player ${player} wins!`)
+        console.log('\n')
+        return playagain()
     }
+    // diagnol right-left win
     if (board[0][2] === board[1][1] && board[0][0] === board[2][0]) {
         console.log(`player ${player} wins!`)
+        console.log('\n')
+        return playagain()
+    }
+    // first row win
+    if (board[0][0] === board[0][1] && board[0][0] === board[0][2]) {
+        console.log(`player ${player} wins!`)
+        console.log('\n')
+        return playagain()
+    }
+    // second row win
+    if (board[1][0] === board[1][1] && board[1][0] === board[1][2]) {
+        console.log(`player ${player} wins!`)
+        console.log('\n')
+        return playagain()
+    }
+    // third row win
+    if (board[2][0] === board[2][1] && board[2][0] === board[2][2]) {
+        console.log(`player ${player} wins!`)
+        console.log('\n')
+        return playagain()
     }
     else {
         decideTurn()
 
     }
-
-
-    // switch (board) {
-    //     case board[0][0] === board[1][0] && board[0][0] === board[2][0]:
-    //         console.log(`Player ${player} is the winner`);
-    //         gameOver === true
-    //         break;
-    //     default:
-    //         console.log('default')
-    // }
 }
 
 function decideTurn() {
     if (playerXTurn) {
+        // switch state to switch turns
         playerXTurn = !playerXTurn;
         playerOTurn = !playerOTurn;
         playerMove(players[0])
     } else {
+        // switch state to switch turns
         playerXTurn = !playerXTurn;
         playerOTurn = !playerOTurn;
         playerMove(players[1])
+    }
+}
+
+async function playagain() {
+    const playerChoice = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'playAgain',
+                message: 'Would you like to play again?',
+                choices: ["Yes", "No"]
+            }
+        ])
+    if (playerChoice.playAgain === "Yes") {
+        startGame()
+    } else {
+        console.log("Thank you for playing!")
+        return
     }
 }
