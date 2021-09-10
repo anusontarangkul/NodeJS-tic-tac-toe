e9e85e8a2177c9995abc696ea71d7c987e9d2558
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-09 20:17:40 -0700

    alternating turns

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 
 let players = ["X", "O"]
+let playerXTurn = false;
+let playerOTurn = false;
 
 let board = [[1, 2, 3],
 [4, 5, 6],
@@ -15,32 +17,41 @@ function startGame() {
     console.log('\n')
     console.log("New Game Of Tic Tac Toe!")
     console.log('\n')
-    let firstPlayer = randomizeStartingPlayer();
-    console.log(`Player ${firstPlayer} goes first.`)
+    randomizeStartingPlayer();
+    // console.log(`Player ${firstPlayer} goes first.`)
 
 
-    firstPlayerMove(firstPlayer)
+    decideTurn();
+
+
+
 
 }
 
 function randomizeStartingPlayer() {
     const randomIndex = Math.floor(Math.random() * 2);
+    if (players[randomIndex] === "X") {
+        playerXTurn = true
+    } else {
+        playerOTurn = true;
+    }
     return players[randomIndex];
 }
 
-function firstPlayerMove(player) {
+async function playerMove(player) {
+    console.log(`Player ${player} turn:`)
     let avaibleMoves = [];
     console.log('\n')
     console.log("", board[0], '\n', board[1], '\n', board[2], '\n')
     for (let row of board) {
         for (let i = 0; i < row.length; i++) {
-            if (row[i] !== "O" || row[i] !== "X") {
+            if (row[i] !== "O" && row[i] !== "X") {
                 avaibleMoves.push(row[i])
             }
         }
     }
 
-    inquirer
+    const inputChoice = await inquirer
         .prompt([
             {
                 type: 'list',
@@ -48,15 +59,63 @@ function firstPlayerMove(player) {
                 message: 'Which spot do you want?',
                 choices: avaibleMoves
             }
-        ]).then(data => {
-            for (let row of board) {
-                for (let i = 0; i < row.length; i++) {
-                    if (row[i] === data.name) {
-                        row[i] = player
-                    }
-                }
+        ])
+    for (let row of board) {
+        for (let i = 0; i < row.length; i++) {
+            if (row[i] === inputChoice.name) {
+                row[i] = player
             }
-            console.log(board)
-        })
+        }
+    }
+    checkWinner(player)
+
 }
 startGame()
+
+function checkWinner(player) {
+
+
+
+
+    if (board[0][0] === board[1][0] && board[0][0] === board[2][0]) {
+        console.log(`player ${player} wins!`)
+    }
+    if (board[0][1] === board[1][1] && board[0][1] === board[2][1]) {
+        console.log(`player ${player} wins!`)
+    }
+    if (board[0][2] === board[1][2] && board[0][2] === board[2][2]) {
+        console.log(`player ${player} wins!`)
+    }
+    if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+        console.log(`player ${player} wins!`)
+    }
+    if (board[0][2] === board[1][1] && board[0][0] === board[2][0]) {
+        console.log(`player ${player} wins!`)
+    }
+    else {
+        decideTurn()
+
+    }
+
+
+    // switch (board) {
+    //     case board[0][0] === board[1][0] && board[0][0] === board[2][0]:
+    //         console.log(`Player ${player} is the winner`);
+    //         gameOver === true
+    //         break;
+    //     default:
+    //         console.log('default')
+    // }
+}
+
+function decideTurn() {
+    if (playerXTurn) {
+        playerXTurn = !playerXTurn;
+        playerOTurn = !playerOTurn;
+        playerMove(players[0])
+    } else {
+        playerXTurn = !playerXTurn;
+        playerOTurn = !playerOTurn;
+        playerMove(players[1])
+    }
+}
