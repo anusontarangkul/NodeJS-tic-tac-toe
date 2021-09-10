c5d8c6fd0b3b99567fc8866b8118097484a7c7bd
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-09 19:09:11 -0700

    player chose spots

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
 const inquirer = require('inquirer');
 
-let players = ["Player 1", "Player 2"]
+let players = ["X", "O"]
+
+let board = [[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]];
 
 function startGame() {
-    let row1 = ['', '', ''];
-    let row2 = ['', '', ''];
-    let row3 = ['', '', ''];
+    let gameOver = false;
 
+
+
+    // console.log("", test[0], '\n', test[1], '\n', test[2], '\n')
     console.log('\n')
     console.log("New Game Of Tic Tac Toe!")
     console.log('\n')
-    let firstPlayer = selectStartingPlayer();
-    console.log(`${firstPlayer} goes first.`)
+    let firstPlayer = randomizeStartingPlayer();
+    console.log(`Player ${firstPlayer} goes first.`)
+
+
+    firstPlayerMove(firstPlayer)
+
+}
+
+function randomizeStartingPlayer() {
+    const randomIndex = Math.floor(Math.random() * 2);
+    return players[randomIndex];
+}
+
+function firstPlayerMove(player) {
+    let avaibleMoves = [];
+    console.log('\n')
+    console.log("", board[0], '\n', board[1], '\n', board[2], '\n')
+    for (let row of board) {
+        for (let i = 0; i < row.length; i++) {
+            if (row[i] !== "O" || row[i] !== "X") {
+                avaibleMoves.push(row[i])
+            }
+        }
+    }
 
     inquirer
         .prompt([
             {
-                type: 'text',
+                type: 'list',
                 name: 'name',
-                message: 'test'
+                message: 'Which spot do you want?',
+                choices: avaibleMoves
             }
-        ])
-}
-
-function selectStartingPlayer() {
-    const randomIndex = Math.floor(Math.random() * 2);
-    return players[randomIndex];
+        ]).then(data => {
+            for (let row of board) {
+                for (let i = 0; i < row.length; i++) {
+                    if (row[i] === data.name) {
+                        row[i] = player
+                    }
+                }
+            }
+            console.log(board)
+        })
 }
 startGame()
